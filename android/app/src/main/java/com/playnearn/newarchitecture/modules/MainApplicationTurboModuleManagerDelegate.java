4e8c91bc211661872f97786b725923d7d2c02efb
Nam Tran <nnam.trn@gmail.com>
2024-07-04 10:42:00 +0700

    update keystore

--- android/app/src/main/java/com/playnearn/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/playnearn/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.awesomeproject.newarchitecture.modules;
+package com.playnearnofa.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
